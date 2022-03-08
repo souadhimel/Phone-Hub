562b6f61c2473323d6dd964a97605f0f3e337018
Souad Himel <souad.anam@gmail.com>
2022-03-08 19:40:15 +0600

    text

--- js/script.js
+++ js/script.js
@@ -1,41 +1,42 @@
 // Selectors
 const form = document.getElementById("form");
-const bookInput = document.getElementById("bookInput");
-const booksLength = document.getElementById("booksLength");
+const phoneInput = document.getElementById("phoneInput");
+const phonesLength = document.getElementById("phonesLength");
 const spinner = document.getElementById("spinner");
-const booksContainer = document.getElementById("booksContainer");
+const phonesContainer = document.getElementById("phonesContainer");
 
-// Fetching books data from API
-function getBooks(term) {
+// Fetching phones data from API
+function getPhones(searchText) {
   spinner.classList.remove("hide");
-  booksContainer.innerHTML = "";
-  booksLength.innerHTML = "";
+  phonesContainer.innerHTML = "";
+  phonesLength.innerHTML = "";
 
-  fetch(`https://openlibrary.org/search.json?q=${term}`)
+  fetch(`https://openapi.programming-hero.com/api/phones?search=$(searchText)`)
     .then((res) => res.json())
     .then((data) => {
-      showBooks(data.docs);
+      console.log(data);
+      showPhones(data.docs);
       spinner.classList.add("hide");
     });
 }
 
-// Add books to the DOM
-function showBooks(data) {
-  booksContainer.innerHTML = "";
-  booksLength.innerHTML = `Number of books found: ${data.length}`;
+// Add phones to the DOM
+function showPhones(data) {
+  phonesContainer.innerHTML = "";
+  phonesLength.innerHTML = `Number of phones found: ${data.length}`;
 
-  data.forEach((book) => {
-    booksContainer.innerHTML += `
+  data.forEach((phone) => {
+    phonesContainer.innerHTML += `
       <div class="col col-md-4 col-lg-3 mb-1">
         <div class="card">
-            <img class="img-fluid"  onerror="this.onerror=null; this.src='https://images.unsplash.com/photo-1532037001556-decb941504a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'"  src="https://covers.openlibrary.org/b/id/${
-              book.cover_i
-            }.jpg" class="card-img-top" alt="book" />
+            <img class="img-fluid"  onerror="this.onerror=null; 
+            this.src='https://images.unsplash.com/photo-1532037001556-decb941504a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'"  
+            src=" https://openapi.programming-hero.com/api/phone/${slug}" class="card-img-top" alt="book" />
             <div class="card-body">
-                <h3 class="card-title"><b>Title</b>:${book.title}</h3>
-                <p><b>Author name</b>:${book.author_name || ""}</p>
-                <p><b>Publisher</b>:${book.publisher || ""}</p>
-                <p><b>Publishing date</b>:${book.first_publish_year || ""}</p>
+                <h3 class="card-title"><b>Title</b>:${phone.brand}</h3>
+                <p><b>Author name</b>:${phone.phone_name || ""}</p>
+                <p><b>Publisher</b>:${phone.slug || ""}</p>
+                // <p><b>Publishing date</b>:${phone.release_date || ""}</p>
             </div>
         </div>
     </div>
@@ -46,10 +47,10 @@ function showBooks(data) {
 // Event listeners
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  if (!bookInput.value) {
+  if (!phoneInput.value) {
     alert("Invalid input.");
   } else {
-    getBooks(bookInput.value.trim());
-    bookInput.value = "";
+    getPhones(phoneInput.value.trim());
+    phoneInput.value = "";
   }
 });
