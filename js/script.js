3cc9766fc21a52628987c6f09cc49afcf5b21ae7
Souad Himel <souad.anam@gmail.com>
2022-03-08 09:11:56 +0600

    modified

--- js/script.js
+++ js/script.js
@@ -1,181 +1,55 @@
-const loadProducts = () => {
-  const data = [
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13 mini",
-      slug: "apple_iphone_13_mini-11104",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-mini.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13 Pro",
-      slug: "apple_iphone_13_pro-11102",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13 Pro Max",
-      slug: "apple_iphone_13_pro_max-11089",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13-pro-max.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 13",
-      slug: "apple_iphone_13-11103",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-13.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12 Pro Max",
-      slug: "apple_iphone_12_pro_max-10237",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro-max-.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12 Pro",
-      slug: "apple_iphone_12_pro-10508",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-pro--.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12",
-      slug: "apple_iphone_12-10509",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 12 mini",
-      slug: "apple_iphone_12_mini-10510",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-12-mini.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone SE (2020)",
-      slug: "apple_iphone_se_(2020)-10170",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-se-2020.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 11 Pro Max",
-      slug: "apple_iphone_11_pro_max-9846",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11-pro.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 11 Pro",
-      slug: "apple_iphone_11_pro-9847",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11-pro-max-.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone 11",
-      slug: "apple_iphone_11-9848",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone XS Max",
-      slug: "apple_iphone_xs_max-9319",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xs-max-new1.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone XR",
-      slug: "apple_iphone_xr-9320",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xr-new.jpg",
-    },
-    {
-      brand: "Apple ",
-      phone_name: "iPhone XS",
-      slug: "apple_iphone_xs-9318",
-      image: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-xs-new.jpg",
-    },
-  ];
-  showProducts(data);
-};
+// Selectors
+const form = document.getElementById("form");
+const bookInput = document.getElementById("bookInput");
+const booksLength = document.getElementById("booksLength");
+const spinner = document.getElementById("spinner");
+const booksContainer = document.getElementById("booksContainer");
 
-// show all product in UI
-const showProducts = (products) => {
-  const allProducts = products.map((pd) => pd);
-  for (const product of allProducts) {
-    const image = product.images;
-    const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-        <div>
-      <img class="product-image" src=${product.image}></img>
+// Fetching books data from API
+function getBooks(term) {
+  spinner.classList.remove("hide");
+  booksContainer.innerHTML = "";
+  booksLength.innerHTML = "";
+
+  fetch(`https://openlibrary.org/search.json?q=${term}`)
+    .then((res) => res.json())
+    .then((data) => {
+      showBooks(data.docs);
+      spinner.classList.add("hide");
+    });
+}
+
+// Add books to the DOM
+function showBooks(data) {
+  booksContainer.innerHTML = "";
+  booksLength.innerHTML = `Number of books found: ${data.length}`;
+
+  data.forEach((book) => {
+    booksContainer.innerHTML += `
+      <div class="col col-md-4 col-lg-3 mb-1">
+        <div class="card">
+            <img class="img-fluid"  onerror="this.onerror=null; this.src='https://images.unsplash.com/photo-1532037001556-decb941504a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=750&q=80'"  src="https://covers.openlibrary.org/b/id/${
+              book.cover_i
+            }.jpg" class="card-img-top" alt="book" />
+            <div class="card-body">
+                <h3 class="card-title"><b>Title</b>:${book.title}</h3>
+                <p><b>Author name</b>:${book.author_name || ""}</p>
+                <p><b>Publisher</b>:${book.publisher || ""}</p>
+                <p><b>Publishing date</b>:${book.first_publish_year || ""}</p>
+            </div>
         </div>
-        <h3>${product.brand}</h3>
-        <p>${product.phone_name}</p>
-        <h2> ${product.slug}</h2>
-      
-        <button onclick="addToCart(${product.brand},${product.slug})" id="addToCart-btn" class="buy-now btn btn-success">Details</button>
-        </div>
-        `;
-    document.getElementById("all-products").appendChild(div);
+    </div>
+    `;
+  });
+}
+
+// Event listeners
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (!bookInput.value) {
+    alert("Invalid input.");
+  } else {
+    getBooks(bookInput.value.trim());
+    bookInput.value = "";
   }
-};
-
-loadProducts();
-
-// let count = 0;
-// const addToCart = (id, price) => {
-//   count = count + 1;
-//   updatePrice("price", price);
-
-//   updateTaxAndCharge();
-//   document.getElementById("total-Products").innerText = count;
-// };
-
-// const getInputValue = (id) => {
-//   const element = document.getElementById(id).innerText;
-//   // console.log(element);
-
-//   const converted = Number(element);
-//   // console.log(converted);
-//   return converted;
-// };
-
-// // main price update function
-// const updatePrice = (id, value) => {
-//   const convertedOldPrice = getInputValue(id);
-//   const convertPrice = parseFloat(value);
-//   // console.log(id, value);
-//   const total = convertedOldPrice + convertPrice;
-//   // console.log(convertedOldPrice, convertPrice);
-//   document.getElementById(id).innerText = total.toFixed(2);
-//   updateTotal();
-// };
-
-// // set innerText function
-// const setInnerText = (id, value) => {
-//   document.getElementById(id).innerText = value.toFixed(2);
-//   // console.log(id, value);
-// };
-
-// // update delivery charge and total Tax
-// const updateTaxAndCharge = () => {
-//   const priceConverted = getInputValue("price");
-//   if (priceConverted > 200) {
-//     setInnerText("delivery-charge", 30);
-//     setInnerText("total-tax", priceConverted * 0.2);
-//   }
-//   if (priceConverted > 400) {
-//     setInnerText("delivery-charge", 50);
-//     setInnerText("total-tax", priceConverted * 0.3);
-//   }
-//   if (priceConverted > 500) {
-//     setInnerText("delivery-charge", 60);
-//     setInnerText("total-tax", priceConverted * 0.4);
-//   }
-// };
-
-// //grandTotal update function
-// const updateTotal = () => {
-//   const grandTotal =
-//     getInputValue("price") +
-//     getInputValue("delivery-charge") +
-//     getInputValue("total-tax");
-//   document.getElementById("total").innerText = grandTotal.toFixed(2);
-// };
-// loadProducts();
+});
